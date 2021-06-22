77d9ad4254889ab7a4bc789fc5f704a88623c5ab
PolMaverick <86291383+PolMaverick@users.noreply.github.com>
2021-06-22 18:04:56 +0900

    Add files via upload

---
+++ day01/src/start/Hi.java
@@ -0,0 +1,12 @@
+package start;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		System.out.println("Hi Class 입니다.");
+		System.out.println("환영합니다.");
+		System.out.println("반갑습니다.");
+
+	}
+
+}
