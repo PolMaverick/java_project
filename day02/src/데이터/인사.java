c7ed325f58f5dba505f047a5a427c4f12e273810
PolMaverick <86291383+PolMaverick@users.noreply.github.com>
2021-06-23 17:57:15 +0900

    Add files via upload

---
+++ day02/src/데이터/인사.java
@@ -0,0 +1,20 @@
+package 데이터;
+
+public class 인사 {
+
+	public static void main(String[] args) {
+		// 주석, comment
+		// -> 단축키 ctrl + /
+		// 자동완성 ctrl + space bar, 저장: ctrl + s, 실행: ctrl + f11 => 번역(컴파일) --> 실행 (run)
+		// sysout, syso
+		// 기본출력장치: 모니터, 기본입력장치: 키보드
+		// . -> 접근연산자(기호) (안으로 들어가는. 생태계의 의미)
+		// 기능은 함수로 나타나게 만든다
+		// 한글자 이상은 이중따옴표 ", 한글자는 싱글따옴표'
+		// 창이 뜨기 전에는 ctrl + 단축키, 창이 뜬 이후에는 alt + 단축키
+		System.out.println("또 만났네요"); // 명령어
+		System.out.println("또또 만났네요");
+
+	}
+
+}
---
+++ day02/src/연산자/산술연산자.java
@@ -0,0 +1,20 @@
+package 연산자;
+
+public class 산술연산자 {
+
+	public static void main(String[] args) {
+		// 비교연산자: == 같다., != (같지않다), >(초과, 미만), >=(이상, 이하)
+		// 비교결과가 중요(boolean, 논리데이터 -> true/false)
+		int x = 100;
+		int y = 200;
+		
+		System.out.println(x == y);
+		System.out.println(x != y);
+		System.out.println(x >= y);
+		System.out.println(x > y);
+		System.out.println(x < y);
+
+
+	}
+
+}
